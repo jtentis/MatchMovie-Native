3ab6c70dc5a7553bc172fd39c4d2e88d99ea0116
João Tentis <101594270+jtentis@users.noreply.github.com>
2024-06-15 23:50:50 -0300

    Feat: Criando tela match

--- app/(tabs)/match.tsx
+++ app/(tabs)/match.tsx
@@ -1,68 +1,186 @@
 import React from 'react';
-import {Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+    Button,
+    Image,
+    ImageBackground,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from 'react-native';
 import {ThemedText} from "@/components/ThemedText";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {Colors} from "@/constants/Colors";
+import Carousel from 'react-native-reanimated-carousel';
 
 export default function MatchScreen() {
-  return (
-      <View
-          style={[
-            styles.container,
-            {
-              // Try setting `flexDirection` to `"row"`.
-              flexDirection: 'column',
-            },
-          ]}>
-        <View style={{
-          flex: 1,
-          backgroundColor: Colors.dark.background,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-          gap: 10,
-        }} >
-          <FontAwesome size={22} name="arrow-left" color={'#FFFFFF'} style={{
-            marginTop:'10%',
-            backgroundColor:Colors.dark.tabIconSelected,
-            padding:15,
-            borderRadius:100,
-          }}/>
-          <ThemedText type="title" style={{marginTop:'10%'}}>Minhas Sessões</ThemedText>
-        </View>
-        <View style={{
-          flex: 3/2,
-          backgroundColor: Colors.dark.background,
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }} >
-          <ScrollView horizontal={true} style={{
-            flex: 1,
-            marginTop: 24,
-            marginLeft: 20,
-          }}>
 
-            <Image style={styles.images} source={require('@/assets/images/movie-poster.jpg')} resizeMode={'center'}/>
-            <Image style={styles.images} source={require('@/assets/images/movie-poster.jpg')} resizeMode={'center'}/>
-            <Image style={styles.images} source={require('@/assets/images/movie-poster.jpg')} resizeMode={'center'}/>
-            <Image style={styles.images} source={require('@/assets/images/movie-poster.jpg')} resizeMode={'center'}/>
-            <Image style={styles.images} source={require('@/assets/images/movie-poster.jpg')} resizeMode={'center'}/>
+    return (
+        <View
+            style={[
+                styles.container,
+                {
+                    // Try setting `flexDirection` to `"row"`.
+                    flexDirection: 'column',
+                },
+            ]}>
+            <View style={{
+                flex: 1,
+                backgroundColor: Colors.dark.background,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+                gap: 10,
+            }}>
+                <FontAwesome size={22} name="arrow-left" color={'#FFFFFF'} style={{
+                    marginTop: '10%',
+                    backgroundColor: Colors.dark.tabIconSelected,
+                    padding: 15,
+                    borderRadius: 100,
+                }}/>
+                <ThemedText type="title" style={{marginTop: '10%'}}>Minhas Sessões</ThemedText>
+            </View>
+            <View style={{
+                flex: 4 / 2,
+                backgroundColor: Colors.dark.background,
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+            }}>
+                <ScrollView horizontal={true} style={{marginLeft: 20}} showsHorizontalScrollIndicator={false}>
+                    <View style={[styles.card, styles.elevated]}>
+                        <Image source={require('@/assets/images/random1.jpg')} resizeMode={"stretch"}
+                               style={styles.images}></Image>
+                        <ThemedText type="defaultSemiBold" style={{
+                            position: 'absolute',
+                            paddingTop: '170%',
+                            paddingRight: '70%'
+                        }}>Escola</ThemedText>
+                    </View>
+                    <View style={[styles.card, styles.elevated]}>
+                        <Image source={require('@/assets/images/random2.jpg')} resizeMode={"stretch"}
+                               style={styles.images}></Image>
+                        <ThemedText type="defaultSemiBold" style={{
+                            position: 'absolute',
+                            paddingTop: '170%',
+                            paddingRight: '60%'
+                        }}>Trabalho</ThemedText>
+                    </View>
+                    <View style={[styles.card, styles.elevated]}>
+                        <Image source={require('@/assets/images/random3.jpg')} resizeMode={"stretch"}
+                               style={styles.images}></Image>
+                        <ThemedText type="defaultSemiBold" style={{
+                            position: 'absolute',
+                            paddingTop: '170%',
+                            paddingRight: '60%'
+                        }}>Família</ThemedText>
+                    </View>
+                    <View style={[styles.card, styles.elevated]}>
+                        <Image source={require('@/assets/images/random 4.jpg')} resizeMode={"stretch"}
+                               style={styles.images}></Image>
+                        <ThemedText type="defaultSemiBold" style={{
+                            position: 'absolute',
+                            paddingTop: '170%',
+                            paddingRight: '60%'
+                        }}>Escola</ThemedText>
+                    </View>
+                    <View style={[styles.card, styles.elevated]}>
+                        <Image source={require('@/assets/images/random1.jpg')} resizeMode={"stretch"}
+                               style={styles.images}></Image>
+                        <ThemedText type="defaultSemiBold" style={{
+                            position: 'absolute',
+                            paddingTop: '170%',
+                            paddingRight: '60%'
+                        }}>Trabalho</ThemedText>
+                    </View>
+                </ScrollView>
+            </View>
+            <View style={{
+                flex: 1,
+                backgroundColor: Colors.dark.background,
+                paddingLeft: 27,
+                paddingTop: 20,
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                justifyContent:'space-between',
+                gap: 10
+            }}>
+                <ThemedText type="subtitle" style={{
+                    position: 'absolute',
+                    paddingTop: 10,
+                    paddingLeft: 30
 
-          </ScrollView>
+                }}>Criar nova sessão</ThemedText>
+                <TextInput
+                    style={styles.input}
+                    value='Nome da Sessão'
+                    placeholder="useless placeholder"
+                    keyboardType="default"
+                />
+            </View>
+            <View style={{
+                flex: 1,
+                backgroundColor: Colors.dark.background,
+                paddingLeft: 27,
+                paddingTop: 20,
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                justifyContent:'space-between',
+                gap: 10
+            }}>
+                <ThemedText type="subtitle" style={{
+                    position: 'absolute',
+                    paddingTop: 10,
+                    paddingLeft: 30
+
+                }}>Entrar em sessão existente</ThemedText>
+                <TextInput
+                    style={styles.input}
+                    value='Endereço da Sessão'
+                    placeholder="useless placeholder"
+                    keyboardType="default"
+                />
+            </View>
         </View>
-        <View style={{flex: 2, backgroundColor: 'green'}} />
-      </View>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  images:{
-    borderRadius: 10,
-    width: 150,
-    height: 200,
-  }
+    container: {
+        flex: 1,
+    },
+    images: {
+        borderRadius: 8,
+        width: 160,
+        height: 230,
+        elevation: 10
+    },
+    boxContainer: {
+        flex: 1,
+    },
+    card: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 160,
+        height: 230,
+        borderRadius: 8,
+        margin: 8,
+        elevation: 10
+    },
+    elevated: {
+        backgroundColor: 'white',
+    },
+    input: {
+        width: 360,
+        height: 50,
+        backgroundColor: Colors.dark.input,
+        padding: 15,
+        marginTop: 30,
+        borderRadius: 8,
+        elevation: 10,
+    }
 });
