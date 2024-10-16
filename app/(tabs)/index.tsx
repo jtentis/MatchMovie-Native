03735f62dee657a6a38a3f24ae7421b0f46f42df
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2024-10-16 13:07:01 -0300

    Style, fix: Adicionando o svg da logo para melhorar visualização

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,6 +17,7 @@ import {
     TouchableWithoutFeedback,
     View
 } from 'react-native';
+import Svg, { G, Path } from 'react-native-svg';
 
 const API_KEY = '2017240ed8d4e61fbe9ed801fe5da25a';
 const BASE_URL = 'https://api.themoviedb.org/3';
@@ -27,6 +28,23 @@ const NOW_PLAYING_MOVIES_URL = `${BASE_URL}/movie/now_playing?api_key=${API_KEY}
 const TOP_RATED_MOVIES_URL = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=pt-BR`;
 const UPCOMING_MOVIES_URL = `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR`;
 const SEARCH_MOVIE_URL = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=pt-BR`;
+
+const Icon = (props:any) => (
+    <Svg
+      viewBox="0 0 34 39"
+      width={54}
+      height={50}
+      {...props}
+    >
+      <G filter="url(#a)">
+        <Path
+          fillRule="evenodd"
+          d="m24.408 1.453-.533 1.382-2.98-.114-2.98-.113V4.9l2.228.136c1.226.074 2.321.224 2.434.332.114.108-.038.754-.336 1.436-.298.681-.493 1.286-.433 1.343.161.154 8.544-1.105 8.766-1.316.168-.16-4.827-6.282-5.404-6.623-.126-.075-.47.486-.762 1.246Zm-11.096.502c-.503.531-.57 1.732-.57 10.212 0 6.187-.123 9.61-.346 9.61-.208 0-.344-1.404-.344-3.55 0-4.07-.393-5.016-2.081-5.016-1.884 0-2.146.912-2.007 7.002.12 5.298.136 5.395 1.257 7.57 1.235 2.394 3 4.068 5.563 5.274 1.303.614 2.151.743 4.855.743 2.939 0 3.482-.1 5.273-.966 2.336-1.13 4.487-3.23 5.53-5.4.67-1.39.742-2.109.746-7.374.005-6.418-.15-6.93-2.093-6.93-1.31 0-1.868.88-1.868 2.944 0 .962-.156 1.75-.345 1.75-.2 0-.345-1.074-.345-2.563 0-2.985-.527-4.026-2.036-4.026-1.582 0-2.103.883-2.103 3.567 0 1.355-.147 2.363-.345 2.363-.201 0-.345-1.106-.345-2.658 0-2.888-.327-3.821-1.488-4.24-.768-.277-1.912.092-2.386.77-.14.2-.256 1.511-.26 2.916-.002 1.479-.15 2.553-.35 2.553-.218 0-.344-2.501-.344-6.845 0-5.661-.093-6.97-.537-7.576-.663-.905-2.233-.972-3.03-.13ZM9.2 5.034l-2.335 1.04-.99-.906c-.544-.497-1.069-.782-1.166-.63-.377.585-2.013 6.764-1.836 6.933.167.16 2.141-.243 6.203-1.264l1.335-.336-.904-.9c-.497-.496-.903-.992-.903-1.103 0-.111.698-.457 1.552-.768 1.469-.536 1.552-.634 1.552-1.84 0-.7-.039-1.271-.087-1.27-.047.002-1.137.472-2.421 1.044Z"
+          clipRule="evenodd"
+        />
+      </G>
+    </Svg>
+  )
 
 interface Movie {
     id: number;
@@ -141,15 +159,15 @@ const App = ({navigation}:{navigation: any}) => {
     };
 
     const getFilterButtonStyle = (filterType: string) => {
-        return filter === filterType
+        return (filter === filterType
             ? [styles.filterButton, styles.filterButtonActive]
-            : styles.filterButton;
+            : styles.filterButton
+        )
     };
 
     return (
         <><View style={styles.searchDiv}>
-            <Image source={require('@/assets/images/logo-clean.png')} style={styles.logo}>
-            </Image>
+            <Icon fill="#D46162" style={styles.icon}/>
             <View style={{flexDirection: 'row'}}>
                 <TextInput
                     style={styles.input}
@@ -177,14 +195,14 @@ const App = ({navigation}:{navigation: any}) => {
                 backgroundColor: Colors.dark.background,
                 paddingBottom: 10
             }}>
-                <ThemedText onPress={() => applyFilter('popular')} type="default"
+                <ThemedText onPress={() => applyFilter('popular')} type="subtitle"
                             style={getFilterButtonStyle('popular')}>Populares</ThemedText>
-                <ThemedText onPress={() => applyFilter('top_rated')} type="default"
+                <ThemedText onPress={() => applyFilter('top_rated')} type="subtitle"
                             style={getFilterButtonStyle('top_rated')}>Mais Votados</ThemedText>
-                <ThemedText onPress={() => applyFilter('now_playing')} type="default"
+                <ThemedText onPress={() => applyFilter('now_playing')} type="subtitle"
                             style={getFilterButtonStyle('now_playing')}>Nos Cinemas</ThemedText>
-                <ThemedText onPress={() => applyFilter('upcoming')} type="default"
-                            style={getFilterButtonStyle('upcoming')}>Em Breve</ThemedText>
+                <ThemedText onPress={() => applyFilter('upcoming')} type="subtitle"
+                            style={getFilterButtonStyle('upcoming')} >Em Breve</ThemedText>
             </View>
             <SafeAreaView style={styles.container}>
                 <FlatList
@@ -227,6 +245,7 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: 8,
         borderTopLeftRadius: 8,
         elevation: 10,
+        marginLeft:55
     },
     button: {
         width: 85,
@@ -245,7 +264,7 @@ const styles = StyleSheet.create({
     },
     filterButton: {
         fontSize: 12,
-        marginTop: 10
+        marginTop: 10,
     },
     gridItem: {
         flex: 1,
@@ -261,6 +280,11 @@ const styles = StyleSheet.create({
         height: 70,
     }, filterButtonActive: {
         color: Colors.dark.tabIconSelected,
+    },
+    icon:{
+        position: 'absolute',
+        top:85,
+        left:18
     }
 });
 export default App;
