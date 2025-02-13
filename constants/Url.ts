707955b36266f9a2f9caeb6bec95bdb8aeace578
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-13 16:08:22 -0300

    URL prod

--- constants/Url.ts
+++ constants/Url.ts
@@ -2,4 +2,4 @@ import Constants from "expo-constants";
 
 const uri =
     Constants.expoConfig?.hostUri?.split(":").shift()?.concat(":3000")
-export const URL_LOCALHOST = `http://${uri}`;
+export const URL_LOCALHOST = `matchmovie-api-production.up.railway.app`;
