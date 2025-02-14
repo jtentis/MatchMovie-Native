eb15b028fe05c5d59de9db4c13b707c280454405
João Tentis <101594270+jtentis@users.noreply.github.com>
2025-02-13 23:47:45 -0300

    Test Expo

--- constants/Url.ts
+++ constants/Url.ts
@@ -1,5 +1,7 @@
+import { EXPO_PUBLIC_URL_RAILWAY } from '@env';
+
 import Constants from "expo-constants";
 
 const uri =
     Constants.expoConfig?.hostUri?.split(":").shift()?.concat(":3000")
-export const URL_LOCALHOST = process.env.EXPO_PUBLIC_URL_RAILWAY;
+export const URL_LOCALHOST = EXPO_PUBLIC_URL_RAILWAY;
